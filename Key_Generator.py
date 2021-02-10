301a7c8db415071db40b7d7b93bcb6bc14c93768
Spoowy <78769981+Spoowy63@users.noreply.github.com>
2021-02-10 20:04:24 +0100

    Create Key_Generator.py

---
+++ Key_Generator.py
@@ -0,0 +1,16 @@
+from random import *
+import string
+
+print("leave empty if you dont use password")
+#should only be letters
+a = input("\nWhat's the pastebin password (only letters): ")
+
+b = int(input("how many passwords do you want: "))
+
+for i in range(b):
+    c = randint(1000,99999)
+    d = ''.join(choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(8))
+    if b is None:
+        print(f"{d}{c}")
+    else:
+        print(f"{d}{c}{a}")
